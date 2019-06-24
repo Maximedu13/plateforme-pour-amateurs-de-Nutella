d6b90ace63a5494ed3cb42c50b4d543cc452f8ff
Maximedu13 <maxime.j.pro@free.fr>
2019-06-24 23:56:31 +0200

    try deploy 8

--- nutella_stop/settings.py
+++ nutella_stop/settings.py
@@ -136,19 +136,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if os.environ.get('ENV') == 'PRODUCTION':
-    
-    # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
