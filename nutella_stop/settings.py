6c13d0e8584cf081450b24c2fb719cf82f2beb8c
Maximedu13 <maxime.j.pro@free.fr>
2019-07-17 14:39:17 +0200

    last fixes heroku

--- nutella_stop/settings.py
+++ nutella_stop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'c@n%u@91tum=@j392g20b8znh7dqfo-v%81))gxbbmu$=dy_*)')
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
